bedf5c6ee5f1e81bec5574241a59368ab36fa711
Dan Sanchez <dansanchez@Dans-MacBook-Pro.local>
2024-04-01 22:13:30 -0600

    adding dynamic force static

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export const metadata: Metadata = {
         },
     },
 }
+export const dynamic = 'force-static'
 
 export default function RootLayout({
                                        children,
