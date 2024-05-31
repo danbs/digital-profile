f62fe7c2dc90db8d65678052a641ff4f190be609
Dan Sanchez <dansanchez@Dans-MacBook-Pro.local>
2024-05-31 06:30:59 -0600

    upgrading template 4

--- src/app/speaking/page.tsx
+++ src/app/speaking/page.tsx
@@ -1,6 +1,6 @@
 import { Card } from "@/components/Card";
 import Section from "@/components/Section";
-import SimpleLayout from "@/components/SimpleLayout";
+import {SimpleLayout} from "@/components/SimpleLayout";
 import React from "react";
 import { DiVim } from "react-icons/di";
 
